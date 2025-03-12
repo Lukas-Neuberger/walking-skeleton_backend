07b274067cdd18640ac447f8bbeba7971bd7e84c
Der wahre Luki <159880557+Lukas-Neuberger@users.noreply.github.com>
2025-03-13 00:53:15 +0100

    feat: add spring boot setup for backend

---
+++ src/test/java/com/example/walking_sekelton/WalkingSekeltonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walking_sekelton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkingSekeltonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
